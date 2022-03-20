4677710e82d69b73192e0d63dda8d99f489784c2
Aditya Gupta <adityagupta765469@gmail.com>
2022-03-20 09:45:08 +0530

    Create Election.sol

---
+++ Election.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.13;
+
+contract ELection{
+
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    uint public candidateCount;
+
+    mapping(uint => Candidate) public candidates;
+
+    mapping(address => bool) public hasVoted;
+
+    event electionUpdated(
+          uint id,
+          string name,
+          uint voteCount
+    );
+
+    constructor(){
+        addCandidate("Donald Trump");
+        addCandidate("Barack Obama");
+    }
+
+    function addCandidate(string memory name) public {
+        candidateCount++;
+        candidates[candidateCount] = Candidate(candidateCount, name, 0);
+    }
+
+    function Vote(uint _id) public{
+        require(!hasVoted[msg.sender], 'you have voted for the participant');
+        require(candidates[_id].id != 0, 'the id doesnt exist');
+        candidates[_id].voteCount +=1;
+        hasVoted[msg.sender] = true;
+        emit electionUpdated(_id, candidates[_id].name, candidates[_id].voteCount);
+    }
+}
