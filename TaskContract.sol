3c3d68fac90882838d27cfb3fd1ee470f122da36
Aditya Gupta <adityagupta765469@gmail.com>
2022-03-25 18:26:25 +0530

    Add files via upload

---
+++ TaskContract.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+contract TaskContract {
+    event AddTask(address recipient, uint taskId);
+    event DeleteTask(uint taskId);
+
+    struct Task{
+        uint id;
+        string taskText;
+        bool isDeleted;
+    }
+
+    Task[] private tasks;
+
+    mapping(uint => address) taskToOwner;
+
+    function addTask(string memory taskText, bool isDeleted) external {
+        uint taskId = tasks.length;
+        tasks.push(Task(taskId, taskText, isDeleted));
+        taskToOwner[taskId] = msg.sender;
+        emit AddTask(msg.sender, taskId);
+    }
+
+    function getMyTasks() external view returns (Task[] memory){
+     Task[] memory temporaryTasks = new Task[](tasks.length);
+        uint count = 0;
+        for(uint i = 0; i < tasks.length; i++){
+            if(tasks[i].isDeleted == false){
+                temporaryTasks[count] = tasks[i];
+                count++;
+            }
+        }
+
+        Task[] memory result = new Task[](count);
+
+        for(uint i = 0; i < count; i++){
+            result[i] = temporaryTasks[i];
+        }
+        return result;
+    }
+
+    function deleteTask(uint taskId) external{
+        if(taskToOwner[taskId] == msg.sender){
+            tasks[taskId].isDeleted = true;
+            emit DeleteTask(taskId);
+        }
+    }
+}
