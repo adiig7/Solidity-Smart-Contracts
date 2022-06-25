23e26e96c3c9a235ade29e3b16651f5d840440e3
Aditya Gupta <adityagupta765469@gmail.com>
2022-06-25 18:46:12 +0530

    Update NFTee.sol

--- NFTee.sol
+++ NFTee.sol
@@ -1,3 +1,6 @@
+// A minimal NFT minting smart contract
+// Deployed to RINKEBY TESTNET: 0x358AA13c52544ECCEF6B0ADD0f801012ADAD5eE3
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
