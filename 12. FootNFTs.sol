33582ff52740319e29e2e94fa1d9e006fcf8860f
Aditya Gupta <adityagupta765469@gmail.com>
2022-07-31 16:26:50 +0530

    Update and rename 12. ChainBattles.sol to 12. FootNFTs.sol

--- 12. FootNFTs.sol
+++ 12. FootNFTs.sol
@@ -1,4 +1,4 @@
-// 0x14b8E77fBf5b5518A134e9c17D193fde8058cB2d
+// 0x595ED8547d82fd0F9916eC4eF61C7c001652bb60
 
 // SPDX-License-Identifier: MIT
 
@@ -9,22 +9,30 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
-contract ChainBattles is ERC721URIStorage{
+contract FootNFTs is ERC721URIStorage{
     using Strings for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    mapping(uint256 => uint256) public tokenIdToLevels;
 
-    constructor() ERC721("Chain Battles", "CHB"){}
+    struct footballPlayer{
+        uint256 ranking;
+        uint256 goals;
+        uint256 numberOfClubs;
+    }
 
-    function generateCharacter(uint256 tokenId) public returns(string memory) {
+    mapping(uint256 => footballPlayer) public tokenIdToPlayer;
+
+
+    constructor() ERC721("Foot NFTs", "FOOT"){}
+
+    function generateCharacter(uint256 tokenId) public view returns(string memory) {
         bytes memory svg = abi.encodePacked(
             '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
             '<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
             '<rect width="100%" height="100%" fill="black" />',
             '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"Warrior",'</text>',
-            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Levels: ",getLevels(tokenId),'</text>',
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Levels: ",getRanking(tokenId),'</text>',
             '</svg>'
         );
         return string(
@@ -35,12 +43,12 @@ contract ChainBattles is ERC721URIStorage{
         );
     }
 
-    function getLevels(uint256 tokenId) public view returns(string memory){
-        uint256 levels = tokenIdToLevels[tokenId];
-        return levels.toString();
+    function getRanking(uint256 tokenId) public view returns(string memory){
+        uint256 ranking = tokenIdToPlayer[tokenId].ranking;
+        return ranking.toString();
     }
 
-    function getTokenURI(uint256 tokenId) public returns (string memory){
+    function getTokenURI(uint256 tokenId) public view returns (string memory){
     bytes memory dataURI = abi.encodePacked(
         '{',
             '"name": "Chain Battles #', tokenId.toString(), '",',
@@ -60,15 +68,18 @@ contract ChainBattles is ERC721URIStorage{
         _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
         _safeMint(msg.sender, newTokenId);
-        tokenIdToLevels[newTokenId] = 0;
+        tokenIdToPlayer[newTokenId].ranking = 0;
+        tokenIdToPlayer[newTokenId].goals = 0;
+        tokenIdToPlayer[newTokenId].numberOfClubs = 0;
+
         _setTokenURI(newTokenId, getTokenURI(newTokenId));
     }
 
-    function train(uint256 tokenId) public{
+    function rankUp(uint256 tokenId) public{
         require(_exists(tokenId));
         require(ownerOf(tokenId) == msg.sender, "Only owner can train their NFT");
-        uint256 currentLevel = tokenIdToLevels[tokenId];
-        tokenIdToLevels[tokenId] = currentLevel+1;
+        uint256 currentLevel = tokenIdToPlayer[tokenId].ranking;
+        tokenIdToPlayer[tokenId].ranking = currentLevel+1;
         _setTokenURI(tokenId, getTokenURI(tokenId));
     }
 }
